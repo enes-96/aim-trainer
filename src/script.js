6eab1e069d52723e2d7054adec1dd22a4956d322
Seyit Enes <seyit7enes@gmail.com>
2022-11-24 23:13:02 +0100

    done

--- src/script.js
+++ src/script.js
@@ -2,9 +2,16 @@
 const target = document.querySelector("#target");
 const score = document.querySelector("#score");
 const time = document.querySelector("#time");
+const modal = document.querySelector("#modal");
+const overlay = document.querySelector("#overlay");
+const scoreResult = document.querySelector("#score-result");
+const scoreDescr = document.querySelector("#result-descr");
+const button = document.querySelector("button");
 
+let seconds = 10;
+let timer;
 let userScore = 0;
-let seconds = 60;
+
 time.innerHTML = seconds;
 
 function randomDivGenerator() {
@@ -14,9 +21,31 @@ function randomDivGenerator() {
   target.style.right = `${Math.floor(Math.random() * 900)}px`;
 }
 
+function timingFunction() {
+  if (seconds > 0) {
+    seconds--;
+    time.innerHTML = seconds;
+  } else {
+    clearInterval(timer);
+    modal.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+    scoreResult.innerHTML = userScore;
+    scoreDescr.innerHTML = `you think you can do better than ${userScore}? try again and find out!`;
+  }
+}
+
 target.addEventListener("click", () => {
+  if (!timer) {
+    timer = window.setInterval(function () {
+      timingFunction();
+    }, 1000);
+  }
   userScore++;
   score.innerHTML = userScore;
   randomDivGenerator();
-  setInterval(());
 });
+
+function reloadGame() {
+  window.location.reload();
+}
+button.addEventListener("click", reloadGame);
